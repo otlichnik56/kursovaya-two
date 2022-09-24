238684eb228c9386935f38ad74fe201b808ddf74
Hyp <otlichnik56@yandex.ru>
2022-09-24 21:37:24 +0500

    базовый c юнит-тестами от 24.09.2022 петёр сказал

--- src/main/java/pro/sky/kursovayatwo/ExamController.java
+++ src/main/java/pro/sky/kursovayatwo/ExamController.java
@@ -1,6 +1,7 @@
 package pro.sky.kursovayatwo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,8 @@ public class ExamController {
         this.examinerService = examinerService;
     }
 
-    @GetMapping(path = "/exam/get")
-    public List<Question> getQuestions(@RequestParam("amount") int amount) {
+    @GetMapping(path = "/exam/get/{amount}")
+    public List<Question> getQuestions(@PathVariable int amount) {
         return examinerService.getRandomQuestion(amount);
     }
 }
